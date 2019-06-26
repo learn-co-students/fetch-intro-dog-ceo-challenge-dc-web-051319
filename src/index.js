df241068e5baf06b94610582c5cc26aa2ff25440
Matt Williams <matt.seongjun@gmail.com>
2019-06-26 17:20:03 -0400

    Initial Commit from @kagada01

--- src/index.js
+++ src/index.js
@@ -1,1 +1,66 @@
-console.log('%c HI', 'color: firebrick')
+
+document.addEventListener("DOMContentLoaded", () => {
+    fetchDogBreeds()
+    fetchDogImages()
+  
+    dogContainer = document.getElementById("dog-image-container")
+    dogBreedList = document.getElementById("dog-breeds")
+  })
+  
+  let dogContainer
+  let dogBreedList
+  
+  function fetchDogImages() {
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+      .then(response => response.json())
+      .then(dogsArray => {
+        dogsArray.message.forEach((dog) => renderDogImages(dog))
+      })
+  }
+  
+  function renderDogImages(dogUrl) {
+    let dogImage = document.createElement("img")
+    dogImage.src = dogUrl
+  // debugger
+    dogContainer.appendChild(dogImage)
+  }
+  
+  function fetchDogBreeds() {
+    fetch('https://dog.ceo/api/breeds/list/all')
+      .then(response => response.json())
+      .then(dogsObj => {
+        let breeds = dogsObj.message
+        for (let breed in breeds) {
+          let subBreeds = []
+          if (breeds[breed].length > 0) {
+            subBreeds = breeds[breed]
+          }
+          renderDogBreeds(breed, subBreeds)
+        }
+      })
+  }
+  
+  function renderDogBreeds(breed, subBreeds) {
+    let breedListItem = document.createElement("li")
+    breedListItem.addEventListener("click", (event) => {
+      breedListItem.style.color.rand
+  })
+      breedListItem.innerText = `${breed}`
+      if (subBreeds.length > 0) {
+        let subBreedList = document.createElement("ul")
+        subBreeds.forEach(subBreed => {
+          let subBreedListItem = document.createElement("li")
+          subBreedListItem.innerText = subBreed
+          subBreedList.appendChild(breedListItem)
+          debugger
+  
+         })
+         breedListItem.appendChild(subBreedList)
+  
+      }
+  
+    // debugger
+      dogBreedList.appendChild(breedListItem)
+  
+  }
+  
