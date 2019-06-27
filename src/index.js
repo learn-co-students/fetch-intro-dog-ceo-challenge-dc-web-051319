d8c297674604c7ef6eb905931d533bacaaeb16ac
keithburgie <krburgie@gmail.com>
2019-06-26 22:36:18 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,66 +1,69 @@
 
-document.addEventListener("DOMContentLoaded", function() {
-  getDogs()
+document.addEventListener("DOMContentLoaded", () => {
+  fetchDogs();
 
-  document.addEventListener('input', function(event) {
-    letter = event.target.value
-    renderDogLi(filterBreed(letter))
+  // Filter by Select Menu
+  document.addEventListener('input', (e) => {
+    letter = e.target.value
+    filterItems(letter)
   })
-})
 
-///////////////////////////////////////////////////////////
+});
+
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
 const breedUrl = 'https://dog.ceo/api/breeds/list/all'
 
-function getDogs() {
-  // Challenge #1
+function fetchDogs() {
   fetch(imgUrl)
   .then(response => response.json())
-  .then(dogsArray => { 
-    dogsArray.message.forEach(dog => renderDogs(dog)) 
+  .then(data => {
+    data.message.forEach(image => {
+      createImage(image)
+    })
   })
 
-  // Challenge #2
   fetch(breedUrl)
   .then(response => response.json())
-  .then(dogsArray => { 
-    let breeds = Object.keys(dogsArray.message)
-    for (i = 0; i < breeds.length; i++) { 
-      renderDogLi(breeds[i])
+  .then(data => {
+    for (let key in data.message) {
+      createListItem(key)
     }
   })
 }
 
-function filterBreed(letter){
-  fetch(breedUrl)
-  .then(response => response.json())
-  .then(dogsArray => { 
-    let breeds = Object.keys(dogsArray.message)
-    let breedFiltered = breeds.filter(breed => {
-      return breed[0][0] === letter
-    })
-    debugger
+function createImage(image){
+  let ul = document.getElementById('dog-image-container')
+  let li = document.createElement('li')
+  let img = document.createElement('img')
+  img.src = image
+  li.appendChild(img)
+  ul.appendChild(li)
+}
+
+function createListItem(text) {
+  let ul = document.getElementById('dog-breeds')
+  let li = document.createElement('li')
+  li.innerText = text
+  li.dataset.filter = text[0]
+  ul.appendChild(li)
+  li.addEventListener('click', changeTextColor)
+}
+
+function changeTextColor(e) {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  e.target.style.color = `rgb(${r},${g},${b})`
+}
+
+function filterItems(letter) {
+  let listItems = document.querySelectorAll('li[data-filter]')
+
+  // "filter" is interchangable with "forEach" here
+  Array.from(listItems).filter(item => {
+    item.style.display = "none"
+    if (item.dataset.filter === letter) {
+      item.style.display = "list-item"
+    }
   })
 }
-
-function renderDogLi(breed) {
-  let dogBreeds = document.getElementById('dog-breeds');
-  let li = document.createElement('li')
-  li.innerText = breed
-  dogBreeds.appendChild(li)
-  li.addEventListener('click', colorChange)
-}
-
-function colorChange(e){
-  e.target.style.color = "#ff0000";
-  // console.log(e.target)
-}
-
-function renderDogs(dogImageSrc) {
-  let dogContainer = document.getElementById('dog-image-container');
-  let image = document.createElement('img')
-  image.src = `${dogImageSrc}`
-  dogContainer.appendChild(image);
-}
-
-
