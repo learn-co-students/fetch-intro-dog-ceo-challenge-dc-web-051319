32fae50fc1e149450d7d108f06806d89096d3053
Emily Bolten <emilybolten@gmail.com>
2019-06-27 09:16:13 -0400

    final commit

--- src/index.js
+++ src/index.js
@@ -29,23 +29,71 @@ function fetchImages() {
 
 //getting breeds of the dogs
 function fetchBreeds() {
+    //where the dog breeds will be displayed
     let breedPlacement = document.getElementById('dog-breeds')
+    //select dog breed to search
+    let select = document.getElementById('breed-dropdown');
+    //button to search the dogs by name
+    let selectDog = document.getElementById('select_dog');
 
     fetch("https://dog.ceo/api/breeds/list/all")
     .then((response) => response.json())
     .then( data => {
 
-        for (let key in data.message) {
-            let li = document.createElement('li')
-            li.innerText = key;
+        selectDog.addEventListener('click',function() {
 
-            breedPlacement.appendChild(li);
-
-            //even listener for each list item to change colors
-            li.addEventListener('click', function() {
-                li.style.color = 'red';
-            });
-        }
+        switch (select.value) {
+            case 'a': //if the select is a
+                for (let key in data.message) {
+                    let li = document.createElement('li')
+                    if (key.startsWith('a')) {
+                        li.innerText = key;
+                        breedPlacement.appendChild(li);
+                    };
+                    li.addEventListener('click',function() {
+                        li.style.color = 'red';
+                    });
+                };
+                break;
+            case 'b': //if the select is b
+                for (let key in data.message) {
+                    let li = document.createElement('li')
+                    if (key.startsWith('b')) {
+                        li.innerText = key;
+                        breedPlacement.appendChild(li);
+                    };
+                    li.addEventListener('click',function() {
+                        li.style.color = 'red';
+                    });
+                 };
+                break;
+            case 'c': //if the select is c
+                for (let key in data.message) {
+                    let li = document.createElement('li')
+                    if (key.startsWith('c')) {
+                        li.innerText = key;
+                        breedPlacement.appendChild(li);
+                    };
+                    li.addEventListener('click',function() {
+                        li.style.color = 'red';
+                    });
+                };
+                break;
+            case 'd': //if the select is d
+                for (let key in data.message) {
+                    let li = document.createElement('li')
+                    if (key.startsWith('d')) {
+                        li.innerText = key;
+                        debugger
+                        breedPlacement.appendChild(li);
+                    };
+                    li.addEventListener('click',function() {
+                        li.style.color = 'red';
+                    });
+                };
+                break;
+            }
+        });
     });
 };
 
