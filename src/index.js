ab4228ee3786dacd76887cad9ebada90f9027df3
tariqjunaid <junaidtariq_91@yahoo.com>
2019-07-03 15:51:43 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ console.log('%c connected', 'color: green');
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
 const breedUrl = 'https://dog.ceo/api/breeds/list/all';
 
-document.addEventListener('DOMContentLoaded', fetchImages);
-document.addEventListener('DOMContentLoaded', fetchBreeds);
+document.addEventListener('DOMContentLoaded', init);
+
+function init() {
+    fetchImages()
+    fetchBreeds()
+}
 
 function fetchImages() {
     let div = document.getElementById('dog-image-container')
@@ -19,19 +23,28 @@ function fetchImages() {
         })
 };
 
-function fetchBreeds() {
-    let ul = document.getElementById('dog-breeds')
+function fetchBreeds(filter = undefined) {
+    let ul = document.querySelector('#dog-breeds')
+    ul.innerHTML = ''
+    let select = document.getElementById('breed-dropdown')
+    select.addEventListener('click', handleDropdown)
     fetch(breedUrl)
         .then(response => response.json())
         .then(data => {
-            for (const message in data.message) {
-                let li = document.createElement('li')
-                li.innerText = message
-                ul.appendChild(li)
+            for (const breed in data.message) {
+                if (!filter || breed.startsWith(filter)) {
+                    let li = document.createElement('li')
+                    li.innerText = breed
+                    ul.appendChild(li)
 
-                li.addEventListener('click', function() {
-                    li.style.color = 'green'
-                })
+                    li.addEventListener('click', function () {
+                        li.style.color = 'green'
+                    })
+                }
             }
         })
 };
+
+function handleDropdown(event) {
+    fetchBreeds(event.target.value)
+}
